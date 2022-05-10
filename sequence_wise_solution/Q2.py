bc412e013dc4e8e6144bef6ac03630979275a587
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-05-10 23:18:19 +0530

    Mergesort updated and O(n) Values

--- sequence_wise_solution/Q2.py
+++ sequence_wise_solution/Q2.py
@@ -1,1 +1,23 @@
 # Merge sort Implemented
+
+from operator import index, le
+
+
+def CreateArray(size = 20, max = 100): # Create a Random array of size 20
+    from random import randint
+    return [randint(0,max) for _ in range(size)]
+
+
+def Merge(a,b):
+    c = [] # output array
+    
+    aindx , bindx = 0,0
+    while aindx < len(a) and bindx < len(b):
+        if a[aindx] < b[bindx]:
+            c.append(a[aindx]) 
+            aindx += 1
+        else:
+            c.append(b[bindx])
+            bindx += 1
+    if aindx ==len(a): c.extend(b[bindx:])
+    else: c.extend(b[bindx:])
