2ccbcce9c292555a97df2dcf7bc392c9a0e76bbc
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-05-19 22:08:01 +0530

    # Minimum no. of Jumps to reach end of an array

---
+++ sequence_wise_solution/Q10.py
@@ -0,0 +1,24 @@
+# Minimum no. of Jumps to reach end of an array
+
+import random
+ra= []
+for i in range(6):
+   ra.append(random.randint(1,10)) 
+print(ra)
+print(len(ra))
+
+def jump(ra:list) -> bool:
+    l=r = 0
+    res = 0
+    
+    while(r < len(ra) -1):
+        farthest = 0
+        for i in range(l,r+1):
+            farthest = max(farthest,i+ra[i])
+        l = r + 1
+        r = farthest
+        res += 1
+    
+    return res
+
+print(jump(ra))            
