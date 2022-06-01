77610b73a01a151e07191ce7f5c16663c49fbdb6
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-06-01 19:55:29 +0530

    formated document

--- sequence_wise_solution/24.py
+++ sequence_wise_solution/24.py
@@ -1,13 +1,11 @@
 from operator import index
 import Impfunc
 
-a = Impfunc.createarray(10,1,10)
-b = Impfunc.createarray(10,1,10)
-c = Impfunc.createarray(10,1,10)
-i,j = 0,0 # b,c
-print(a,b,c)
+a = Impfunc.createarray(10, 1, 10)
+b = Impfunc.createarray(10, 1, 10)
+c = Impfunc.createarray(10, 1, 10)
+i, j = 0, 0  # b,c
+print(a, b, c)
 for a in a:
     if a in b and a in c:
         print(f"{a} is found in {b} and {c} at index {b.index(a)} and {c.index(a)} ")
-    
-    
