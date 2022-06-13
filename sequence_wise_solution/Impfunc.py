c1a134bf0cbbf14590ea337a31e5f3e0c51e157e
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-06-13 13:38:37 +0530

    1.binary_search within list 2.print all variable

--- sequence_wise_solution/Impfunc.py
+++ sequence_wise_solution/Impfunc.py
@@ -4,16 +4,17 @@ def createarray(len: int , starting:int , finish:int) -> list:
     for i in range(len):
         arr.append(random.randint(starting,finish))
     return arr
-def binary_search(a: list,target) -> int :
+def binary_search(a: list,strt_indx:int,last_indx:int,target) -> int :
     '''
     1. sorts input arr
     2. Binary serach
     3. return index of target num
     4. if not found return -1
+    5. added strt and last index , to search within list
     '''
     a.sort()
     print(f"{a} --> sorted input array")
-    l,r = 0,len(a)
+    l,r = strt_indx,last_indx
     
     while l<=r:
         m = (l+r)//2
@@ -29,6 +30,19 @@ def ispalindrome(a) -> bool:
        compare with original'''
     a = str(a)
     return a == a[::-1] 
+
+def pav():
+    '''Print All Variables in this .py file'''
+    all_variables = dir()
+  
+    # Iterate over the whole list where dir( )
+    # is stored.
+    for name in all_variables:
+    
+    # Print the item if it doesn't start with '__'
+        if not name.startswith('__'):
+            myvalue = eval(name)
+            print(name, "is", type(myvalue), "and is equal to ", myvalue)
 
 
 def iseven(a) -> bool:
