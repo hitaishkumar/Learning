b3279b17792ab7c7c6d51b76a8cccf60ea692a6d
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-06-15 15:44:30 +0530

    remove duplicates from sorted array

---
+++ sequence_wise_solution/TCS_Ques/6.py
@@ -0,0 +1,8 @@
+# https://takeuforward.org/data-structure/remove-duplicates-in-place-from-sorted-array/
+import Impfunc as im
+
+a = im.createarray()
+print(a)
+
+print(set(sorted(a))) # answer
+print(a)
