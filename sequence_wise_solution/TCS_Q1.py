48c17d13aa20478db2c7ce77f10e3c7c5e4461e3
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-06-15 17:04:28 +0530

    solved in  easier way and added it as a function.

--- sequence_wise_solution/TCS_Q1.py
+++ sequence_wise_solution/TCS_Q1.py
@@ -12,17 +12,30 @@ matrix = [1  2  9  10 25]
 r,c = input(" forst enter row then column").split()
 idxr = int(r)-1
 idxc = int(c)-1
-max = max(idxr,idxc)
+maxii = max(idxr,idxc)
 ap = []
-for i in range(1,max+1):
+for i in range(1,maxii+1):
     ap.append(i*2) 
-
+# print(ap)
 corner_element = sum(ap)+1
-print(corner_element)
+print(corner_element ,"--->", " corner element")
 
 if (idxc % 2 != 0):
     result = corner_element +(idxr-idxc)
 else:
     result = corner_element + (idxc - idxr)
-print(result)
-    
+print(result ,"--->", " prev result")
+
+# more concise and easy to read version
+def rowcol(row:int,col : int):
+    ''' read elemnt in zig zig filled 2D matrix
+        given the row and column
+    '''
+    maxi = max(row,col)
+    corner = (maxi**2) - maxi +1
+    if maxi % 2==0:
+        res = corner - (maxi - row)
+    else:
+        res = corner + (maxi - row)
+    return res 
+print(rowcol(4,4))
