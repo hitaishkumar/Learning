b179f2bc0c74629bfe6a43e187d3cb0415d7d50c
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-06-15 19:13:10 +0530

    added return stat. to func , doc string

--- sequence_wise_solution/TCS_Ques/3.py
+++ sequence_wise_solution/TCS_Ques/3.py
@@ -6,12 +6,12 @@ a = "yaallahbitch"
 b = list(a)
 
 def revarray(a:list , start:int ,end: int) -> int:
-    
+    ''' reverses a array recursively'''
     if(start < end):
         a[start] , a[end] = a[end], a[start]
         # print(a)
         revarray(a,start + 1 , end -1)
+    return a
    
-revarray(b, 0 , len(b)-1)        
-print(b)
+print(revarray(b, 0 , len(b)-1))
 
