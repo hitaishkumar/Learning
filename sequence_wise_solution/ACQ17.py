75ebedd9f5fd2b7125f3663c8274e4c966758289
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-06-15 19:20:37 +0530

    formatted doc string

--- sequence_wise_solution/ACQ17.py
+++ sequence_wise_solution/ACQ17.py
@@ -1,4 +1,5 @@
-#  Write a program in C such that it takes a lower limit and upper limit as inputs and print all the intermediate palindrome numbers.
+#  Write a program in C such that it takes a lower limit 
+# and upper limit as inputs and print all the intermediate palindrome numbers.
 import Impfunc as imp
 def findpal(low:int, up:int) -> int:
     for i in range(low,up+1):
