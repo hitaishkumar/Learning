d5c694f4147b51f6c8b0acb4e099f503c8ec8bf7
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2022-09-10 17:55:43 +0530

    used round and sum

--- MY_SQL/substr.sql
+++ MY_SQL/substr.sql
@@ -5,3 +5,9 @@ SELECT DISTINCT CITY FROM STATION WHERE SUBSTR(CITY,1,1) IN ('A','E','I','O','U'
 SELECT DISTINCT CITY FROM STATION WHERE (SUBSTR(CITY,1,1) IN ('A','E','I','O','U') and SUBSTR(CITY,-1,1) IN ('A','E','I','O','U') ) 
 --learned order by
 select name from employee order by name
+
+-- Query the following two values from the STATION table:
+    --The sum of all values in LAT_N rounded to a scale of  decimal places.
+    --The sum of all values in LONG_W rounded to a scale of  decimal places.
+
+select round(sum(lat_n),2) , round(sum(long_w),2) from station
